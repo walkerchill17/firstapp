cd03a285ba390cdb696f5a208019e7e6b6a33ab6
walkerchill17 <walkerchill9@gmail.com>
2023-02-17 09:12:05 -0500

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/myfirstapp/Myfirstapp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstapp;
+
+import com.mycompany.myfirstapp.GUI.Logic.firstpage;
+
+/**
+ *
+ * @author walke
+ */
+public class Myfirstapp {
+
+    public static void main(String[] args) {
+       
+        firstpage princ = new firstpage();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
---
+++ src/main/java/com/mycompany/myfirstapp/logica/Duenio.java
@@ -0,0 +1,28 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.myfirstapp.logica;
+
+/**
+ *
+ * @author walke
+ */ 
+public class Duenio {
+    
+    private int id_duenio;
+    private String name_duenio;
+    private String cel_duenio;
+
+    public Duenio() {
+    }
+
+    public Duenio(int id_duenio, String name_duenio, String cel_duenio) {
+        this.id_duenio = id_duenio;
+        this.name_duenio = name_duenio;
+        this.cel_duenio = cel_duenio;
+    }
+    
+    
+    
+}
